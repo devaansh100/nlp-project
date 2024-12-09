417d28f5b5dfdff927bbc714ecf297a2d67abdd2
devaansh100 <devaanshgupta100@gmail.com>
2024-12-09 01:16:07 -0800

    edit

--- eval_tqa.py
+++
@@ -1,9 +0,0 @@
-import torch
-from transformers import LlamaForCausalLM, AutoTokenizer
-from truthful_model import TruthfulLlama
-from datasets import load_dataset
-
-
-model = TruthfulLlama(LlamaForCausalLM.from_pretrained('meta-llama/Llama-3.2-1B'))
-tokenizer = AutoTokenizer.from_pretrained('meta-llama/Llama-3.2-1B')
-dataset = load_dataset("truthful_qa", "generation")['validation']
